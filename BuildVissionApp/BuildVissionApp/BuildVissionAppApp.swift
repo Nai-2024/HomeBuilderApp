1e3baa3504f18aa871fc73d813197dbcf08b4167
Din <Din.Salehy@triosstudent.com>
2025-02-10 13:29:14 -0500

    Capstoe Project

---
+++ BuildVissionApp/BuildVissionApp/BuildVissionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildVissionAppApp.swift
+//  BuildVissionApp
+//
+//  Created by Din Salehy on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct BuildVissionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
